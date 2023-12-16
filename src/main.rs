be54f793742952324d3a51c25e19d7f25c6db38d
Douglas Poma <douglaspoma@yahoo.com>
2023-12-16 12:23:19 -0300

    feat: add CRUD endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,73 @@
 use rocket::{
-    get, routes,
+    delete, get, post, put,
+    response::status,
+    routes,
     serde::json::{json, Value},
 };
 
-#[get("/")]
-fn hello() -> Value {
-    json!({"message":"Hello, Mundo!"})
+#[get("/rustaceans")]
+fn get_rustaceans() -> Value {
+    json!([
+        {
+            "id": 1,
+            "name": "John Doe"
+        },
+        {
+            "id": 2,
+            "name": "Jane Doe"
+        }
+    ])
+}
+
+#[get("/rustaceans/<id>")]
+fn view_rustacean(id: i32) -> Value {
+    json!([
+        {
+            "id": id,
+            "name": "John Doe",
+            "email": "john@doe.com"
+        }
+    ])
+}
+
+#[post("/rustaceans", format = "json")]
+fn create_rustacean() -> Value {
+    json!([
+        {
+            "id": 3,
+            "name": "John Connor",
+            "email": "john@connor.com"
+        }
+    ])
+}
+#[put("/rustaceans/<id>", format = "json")]
+fn update_rustacean(id: i32) -> Value {
+    json!([
+        {
+            "id": id,
+            "name": "John Alterado",
+            "email": "john@connor.com"
+        }
+    ])
+}
+#[delete("/rustaceans/<_id>")]
+fn delete_rustacean(_id: i32) -> status::NoContent {
+    status::NoContent
 }
 
 #[rocket::main]
 async fn main() {
-    let _ = rocket::build().mount("/", routes![hello]).launch().await;
+    let _ = rocket::build()
+        .mount(
+            "/",
+            routes![
+                get_rustaceans,
+                view_rustacean,
+                create_rustacean,
+                update_rustacean,
+                delete_rustacean
+            ],
+        )
+        .launch()
+        .await;
 }
