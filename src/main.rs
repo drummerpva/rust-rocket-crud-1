d0c047d25dcdfbe42acd16d690cefe0c5aedc43f
Douglas Poma <douglaspoma@yahoo.com>
2023-12-21 10:03:24 -0300

    feat: run migrations on up app

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,23 @@ mod schema;
 use crate::routes::{
     create_rustacean, delete_rustacean, get_rustaceans, update_rustacean, view_rustacean,
 };
-use rocket::{catchers, routes};
+use rocket::{catchers, fairing::AdHoc, routes, Build, Rocket};
 use routes::{invalid_param, not_found, unauthorized, DbConn};
+
+async fn run_db_migrations(rocket: Rocket<Build>) -> Rocket<Build> {
+    use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+    const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
+    DbConn::get_one(&rocket)
+        .await
+        .expect("Unable to retrieve connection")
+        .run(|connection| {
+            connection
+                .run_pending_migrations(MIGRATIONS)
+                .expect("Unable to run migrations");
+        })
+        .await;
+    rocket
+}
 
 #[rocket::main]
 async fn main() {
@@ -24,6 +39,7 @@ async fn main() {
             ],
         )
         .attach(DbConn::fairing())
+        .attach(AdHoc::on_ignite("Diesel Migrations", run_db_migrations))
         .launch()
         .await;
 }
